a9814244407aa7f2583126eaa097d70dd0640fea
Tobias Klein <kleintobias2004@gmail.com>
2021-12-30 19:14:27 +0100

    First Home-Info-Card added (Balli). Card is also nicely animated (swipes to the left using js an css).

--- js/script.js
+++ js/script.js
@@ -11,4 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#About_Button').onclick = function() {
         window.location.href = 'about.html';
     };
+    document.querySelector('#balli_sec').onclick = function() {
+        let balli_sec = document.getElementById('balli_sec');
+        balli_sec.style.backgroundColor = '#fff';
+        balli_sec.style.marginLeft = '-80%';
+        setTimeout(() => {
+            window.location.href = 'balli.html';
+        }, 1000);
+    };
 });
