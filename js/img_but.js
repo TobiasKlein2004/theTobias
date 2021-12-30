9923f02486aa22ed9768e19aa631e21d70c2647f
Tobias Klein <kleintobias2004@gmail.com>
2021-12-31 00:47:17 +0100

    Content added to Balli-page. Nice BG added. Over the nav-Buttons a littele dynamic text shows you where you are

---
+++ js/img_but.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#img_but1').onclick = function() {
+        window.location.href = 'https://www.youtube.com';
+    }
+    document.querySelector('#img_but2').onclick = function() {
+        window.location.href = 'https://cs50.harvard.edu/x/2021/psets/8/homepage/';
+    }
+});
--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
+    var url = window.location.pathname;
+    let sections = document.getElementById('content');
     document.querySelector('#Home_Button').onclick = function() {
         window.location.href = 'index.html';
     };
     document.querySelector('#Balli_Button').onclick = function() {
-        window.location.href = 'balli.html';
+        if (url == '/index.html') {
+            sections.style.marginLeft = '-100%';
+            setTimeout(() => {
+                window.location.href = 'balli.html';
+            }, 500);
+        } else {
+            window.location.href = 'balli.html';
+        }
     };
     document.querySelector('#Stuff_Button').onclick = function() {
-        window.location.href = 'stuff.html';
+        if (url == '/index.html') {
+            sections.style.marginLeft = '-100%';
+            setTimeout(() => {
+                window.location.href = 'stuff.html';
+            }, 500);
+        } else {
+            window.location.href = 'stuff.html';
+        }
     };
     document.querySelector('#About_Button').onclick = function() {
-        window.location.href = 'about.html';
+        if (url == '/index.html') {
+            sections.style.marginLeft = '-100%';
+            setTimeout(() => {
+                window.location.href = 'about.html';
+            }, 500);
+        } else {
+            window.location.href = 'about.html';
+        }
     };
     document.querySelector('#balli_sec').onclick = function() {
         let balli_sec = document.getElementById('balli_sec');
-        balli_sec.style.backgroundColor = '#fff';
+        balli_sec.style.backgroundColor = 'rgba(255, 255, 0, .5)';
         balli_sec.style.marginLeft = '-80%';
         setTimeout(() => {
             window.location.href = 'balli.html';
-        }, 1000);
+        }, 500);
     };
 });
